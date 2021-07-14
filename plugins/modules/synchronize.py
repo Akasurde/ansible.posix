2aebdb06cdf6bb16ca9ce0fd0ca7de38eaf3cd54
Abhijeet Kasurde <akasurde@redhat.com>
2021-07-14 19:26:45 +0530

    [WIP] synchronize: Use SSH args from SSH connection plugins
    SSH configuration migrated from Ansible configuration to
    SSH connection configuration. Make ``synchronize`` understand
    this.
    
    Fixes: #222
    
    Signed-off-by: Abhijeet Kasurde <akasurde@redhat.com>

--- plugins/modules/synchronize.py
+++ plugins/modules/synchronize.py
@@ -137,7 +137,9 @@ options:
     default: yes
   use_ssh_args:
     description:
-      - Use the ssh_args specified in ansible.cfg. Setting this to `yes` will also make `synchronize` use `ansible_ssh_common_args`.
+      - In Ansible 2.10 and lower, it uses the ssh_args specified in C(ansible.cfg).
+      - In Ansible 2.11 and onwards, it uses SSH connection configurations.
+      - Setting this to C(yes) will also make M(ansible.posix.synchronize) use C(ansible_ssh_common_args).
     type: bool
     default: no
   ssh_connection_multiplexing:
